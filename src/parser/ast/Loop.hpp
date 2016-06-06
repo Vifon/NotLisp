033076fbcb826f24d102b3a7e28ed1c0b01a563a
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2016-06-06 02:17:14 +0200

    One-line loops with correct iterator scoping
    Previously the iterator was stored in the loop's block which might not exist.

--- src/parser/ast/Loop.hpp
+++ src/parser/ast/Loop.hpp
@@ -13,8 +13,8 @@ namespace ast {
 class Loop : public Node
 {
   public:
-    Loop(const std::string& iterator, NodePtr&& collection, NodePtr&& block)
-        : iterator(std::move(iterator))
+    Loop(const std::string& iterator_name, NodePtr&& collection, NodePtr&& block)
+        : iterator_name(std::move(iterator_name))
         , collection(std::move(collection))
         , block(std::move(block))
     {
@@ -27,16 +27,25 @@ class Loop : public Node
         auto list = collection->evaluate();
 
         for (auto& i : list->asList()) {
-            block->addVar(iterator);
-            block->lookup(iterator) = i;
+            iterator = i;
             block->evaluate();
         }
 
         return ValuePtr{new VoidValue};
     }
 
+    ValuePtr& lookup(const std::string& varname) override
+    {
+        if (varname == iterator_name) {
+            return iterator;
+        } else {
+            return Node::lookup(varname);
+        }
+    }
+
   private:
-    std::string iterator;
+    std::string iterator_name;
+    ValuePtr iterator;
     NodePtr collection;
     NodePtr block;
 };
