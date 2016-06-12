685571842893b9e3ac2e7f890cf25f35831275a4
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2016-06-12 13:37:56 +0200

    Remove the old example from parser

--- src/parser/main.cpp
+++ src/parser/main.cpp
@@ -8,32 +8,6 @@
 
 int main(int argc, char *argv[])
 {
-    /*
-    const char* PROGRAM = R"(
-let v = 10;
-let v2 = 6;
-for (i in [1, 2, 3]) {
-  let v3 = i + v;
-  v2 = v2 + 2;
-  print(i);
-  print(v);
-  print(v2);
-  print(v3);
-  if (v == v2) {
-    print(42);
-  }
-}
-
-for (i in [1, 2, 3]) {
-  print(v);
-  let v = i + v;
-  print(i);
-  print(v);
-}
-
-for (i in [1, 4, 8]) print(i);
-)";
-    /*/
     const char* PROGRAM = R"(
 let outer = 100;
 
@@ -84,7 +58,6 @@ print(pow(25));
 
 print(map(pow, l));
 )";
-    //*/
 
     std::stringstream stream{PROGRAM};
     vfn::Parser p{std::make_unique<vfn::Lexer>(stream)};
